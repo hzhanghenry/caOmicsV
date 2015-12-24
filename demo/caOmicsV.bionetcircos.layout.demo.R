2b688dad5b4e1965ff9ecbcb9ce5967da4f60848
hzhanghenry <hzhang@mail.nih.gov>
2015-12-24 11:31:38 -0500

    Update caOmicsV.bionetcircos.layout.demo.R
    Changed demo data name

--- demo/caOmicsV.bionetcircos.layout.demo.R
+++ demo/caOmicsV.bionetcircos.layout.demo.R
@@ -7,12 +7,12 @@
 
 
 library(caOmicsV)
-data(caOmicsV.bionet.eset)
+data(bionetPlotDemoData)
 
 pdf("caOmicsV.bioNetCircos.Layout.Plot.pdf", height=8, width=11)
 
-plotBioNetCircos(caOmicsV.bionet.eset)
+plotBioNetCircos(bionetPlotDemoData)
 dataNames <- c("Tissue Type", "RNASeq", "miRNASeq", "Methylation", "CNV")
-    bioNetLegend(dataNames, heatmapMin=-3, heatmapMax=3)
+bioNetLegend(dataNames, heatmapMin=-3, heatmapMax=3)
 
 dev.off()
